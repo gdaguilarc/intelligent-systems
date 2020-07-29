b574cebadd5303e6e8d244aef8f3b1de02a9c43e
gdaguilarc <gd.aguilarc@gmail.com>
2020-07-29 01:12:51 -0500

    image crop solved

--- main_image.py
+++ main_image.py
@@ -6,14 +6,6 @@ import matplotlib.pyplot as plt
 from sklearn.cluster import AgglomerativeClustering
 
 
-new_img = [[255 for _ in range(28)] for _ in range(28)]
-for i in range(28):
-    new_img[i][i] = 0
-
- # mg = Image.fromarray(np.array(new_img))
-# img.convert("L").show()
-
-
 image = Image.open("numeros.jpeg")
 gray = ImageOps.grayscale(image)
 data = np.asarray(gray)
@@ -28,8 +20,39 @@ for i in range(maxX):
 
 points = np.array(points)
 
-dbscan = DBSCAN(eps=3.8, min_samples=1)
+dbscan = KMeans(n_clusters=11)
 clusters = dbscan.fit_predict(points)
-plt.scatter(points[:, 1], points[:, 0], c=clusters, cmap='plasma')
 
+
+positions = np.where(clusters == 8)
+prov = points[positions][:]
+
+# plt.scatter(prov[:, 1], prov[:, 0], cmap='plasma')
+# plt.show()
+
+# get max and mins
+y_max = prov[:, 0].max()
+x_max = prov[:, 1].max()
+y_min = prov[:, 0].min()
+x_min = prov[:, 1].min()
+area = (x_min, y_min, x_max, y_max)
+
+img = Image.fromarray(data)
+plt.imshow(img)
 plt.show()
+print(area)
+img = img.crop(area)
+img.show()
+
+print(data.shape)
+# print(area)
+# print(x_max)
+# print(image.size)
+# image = data.crop(area)
+# plt.imshow(data)
+# plt.show()
+# print(area)
+# print(image.size)
+# ola = gray.crop(area)
+
+# ola.show()
